a4fa334956f668ebd6967d02567e956b98023fb0
ialom <ifteher.alom@gmail.com>
2019-02-18 22:08:27 +0600

    good commit

--- startTest.sh
+++ startTest.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 #
-# Exit on first error
-set -e
 #
-#
-node benchmark/simple/main.js -c config.yaml -n ../../network/fabric-v1.2/fabric-node.json
+node benchmark/simple/main.js -c config.yaml -n ../../network/fabric-v1.2/dev/fabric-go.json
